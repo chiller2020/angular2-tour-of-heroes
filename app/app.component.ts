abccb9bb8456e9b05fdfcdbc08ea084d5493b088
oddskill <oddskill@gmail.com>
2016-03-05 17:13:08 +0100

    finished part 1 of tutorial
    finished part 1 of tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,24 @@
 import {Component} from 'angular2/core';
-
 interface Hero {
   id: number;
   name: string;
 }
-
 @Component({
-    selector: 'my-app',
-    template:`
+  selector: 'my-app',
+  template:`
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
     `
-
 })
-
 export class AppComponent {
-
   public title = 'Tour of Heroes';
   public hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
 }
