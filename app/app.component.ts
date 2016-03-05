3cd6bac586b70f320f0ca494d953c213910c01d3
oddskill <oddskill@gmail.com>
2016-03-05 17:10:21 +0100

    add interface and enhance template
    add interface and enhance template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,27 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+    `
+
 })
 
 export class AppComponent {
 
   public title = 'Tour of Heroes';
-  public hero = 'Windstorm';
- }
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+}
