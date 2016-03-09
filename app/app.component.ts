2671cfbb1f820de2f761eb4e2157079995934223
oddskill <oddskill@gmail.com>
2016-03-09 17:02:49 +0100

    finished part 3 of tutorial
    finished part 3 of tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,11 @@
 import {Component} from 'angular2/core';
-interface Hero {
-  id: number;
-  name: string;
-}
-@Component({ 
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+@Component({
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
-    <h2>My Lala Heroes</h2>
+    <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="#hero of heroes"
         [class.selected]="hero === selectedHero"
@@ -15,14 +13,7 @@ interface Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles:[`
     .selected {
@@ -72,7 +63,8 @@ interface Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
